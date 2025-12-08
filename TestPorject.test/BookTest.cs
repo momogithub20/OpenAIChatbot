bf812347090b13cdcb48deef7c42013344461047
HamidSirat20 <abeshaqzada@gmail.com>
2025-12-09 01:35:02 +0200

    Test added.

---
+++ TestPorject.test/BookTest.cs
@@ -0,0 +1,13 @@
+﻿namespace TestPorject.test;
+
+public class BookTest
+{
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        int num = 1;
+        Assert.Equal(1, num);
+
+    }
+}
