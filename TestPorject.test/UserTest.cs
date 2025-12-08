090b1b9b1430eb3c67a0b92c60cdc29d39c1c5a0
HamidSirat20 <abeshaqzada@gmail.com>
2025-12-09 01:42:12 +0200

    Added Test

---
+++ TestPorject.test/UserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPorject.test;
+
+public class UserTest
+{
+    [Fact]
+    public void TestUser()
+    {
+        Assert.Equal(2,3); 
+    }
+}
